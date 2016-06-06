dae8fb2a9dfad9a5e54a77c70929a4fbcac4c3bd
user.name <hhxnbw@163.com>
2016-06-06 21:40:51 +0800

    fixing bug

--- app/src/main/java/me/pkhope/jianwei/MainActivity.java
+++ app/src/main/java/me/pkhope/jianwei/MainActivity.java
@@ -1,11 +1,13 @@
 package me.pkhope.jianwei;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.sina.weibo.sdk.auth.AuthInfo;
+import com.sina.weibo.sdk.auth.Oauth2AccessToken;
 import com.sina.weibo.sdk.auth.sso.SsoHandler;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,16 +19,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AuthInfo authInfo = new AuthInfo(this, Constants.APP_KEY, Constants.REDIRECT_URL, Constants.SCOPE);
-        ssoHandler = new SsoHandler(MainActivity.this, authInfo);
+        Oauth2AccessToken token = AccessTokenPreference.loadAccessToken(MainActivity.this);
+        if (token.getToken().equals("")){
 
-        Button login = (Button)findViewById(R.id.login);
+            AuthInfo authInfo = new AuthInfo(this, Constants.APP_KEY, Constants.REDIRECT_URL, Constants.SCOPE);
+            ssoHandler = new SsoHandler(MainActivity.this, authInfo);
+            ssoHandler.authorize(new AuthListener(MainActivity.this));
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ssoHandler.authorize(new AuthListener(MainActivity.this));
-            }
-        });
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(ssoHandler != null){
+            ssoHandler.authorizeCallBack(requestCode, resultCode, data);
+        }
     }
 }
